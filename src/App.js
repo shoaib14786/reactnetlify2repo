08b139feaf6595fd18d9acba94a9d7bd24e3a44b
shoaib14786 <shoaib14786@gmail.com>
2024-07-14 20:03:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my react netlify 2
         </a>
       </header>
     </div>
